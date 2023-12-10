a036024daf7443505c498ae0174bb11505d93e0b
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-12-11 01:22:24 +0330

    add NLP section

--- src/components/organisms/StepsData.tsx
+++ src/components/organisms/StepsData.tsx
@@ -1,9 +1,10 @@
 import { Box, Stack, Typography } from "@mui/material";
-import MLImage from 'assets/ml-image.png';
-import CVImage from 'assets/computer-vision-image.png';
+import { Step } from "components/classes/Step";
 import Image from "next/image";
 import PlayGameButton from "components/molecules/PlayGameButton";
-import { Step } from "components/classes/Step";
+import MLImage from 'assets/ml-image.png';
+import CVImage from 'assets/computer-vision-image.png';
+import NLPImage from 'assets/natural-lanquage-processing-image.png';
 
 const ArtificalInteligence = new Step({
   id: "ArtificalInteligence",
@@ -15,33 +16,33 @@ const ArtificalInteligence = new Step({
       <Typography variant="h1" gutterBottom>{'هوش مصنوعی'}</Typography>
       <Stack alignItems={'end'}>
         <Typography variant="h5" justifyItems={'start'} gutterBottom>{'Artificial Inteligence'}</Typography>
-        <Typography width={600} paddingLeft={18}>
+        <Typography width={660}>
           هوش مصنوعی از اواسط قرن بیستم مورد توجه جدی دانشمندان و سرمایه‌گذاران قرار گرفت؛ اما نتایجِ به دست آمده از تحقیقات و پژوهش‌های کلان، با پیش‌بینی‌های خوش‌بینانه و انتظارات فزاینده‌ی آنان فاصله‌ی زیادی داشت. این شکست، سرانجام پس از مدت کوتاهی و در اواخر قرن بیستم منجر به فروپاشی صنایع وابسته و قطع بودجه‌های پژوهشی در این حوزه شد. این شرایط که حدود سه دهه ادامه داشت بعدها به زمستان هوش مصنوعی مشهور شد. آنچه در درخت هوش مصنوعی می‌بینید مروری بر ابداعات و ابتکاراتی ا‌ست که در قرن بیست و یکم، هوش مصنوعی را از  زمستان خود به بهاری  پربار  رسانده است.
         </Typography>
       </Stack>
     </Stack>,
   links: [
-    { x: 960, y: 330, rotate: 0, length: 540 },
-    { x: 200, y: 290, rotate: -140, length: 400 },
+    { x: 930, y: 330, rotate: 0, length: 630 },
+    { x: 200, y: 290, rotate: -140, length: 480 },
+    { x: 190, y: 600, rotate: 120, length: 400 },
   ]
 })
-
 
 const MachineLearning = new Step({
   parent: ArtificalInteligence,
   id: "MachineLearning",
-  x: 4800,
-  y: 0,
+  x: 4600,
+  y: 240,
   scale: 0.66,
   children:
     <Stack alignItems={'end'} justifyContent={'center'} width={'100%'} height={'100%'}>
       <Typography variant="h2" gutterBottom>{'یادگیری ماشین'}</Typography>
       <Typography variant="h5" gutterBottom>{'Machine Learning'}</Typography>
-      <Typography width={600}>
+      <Typography width={700}>
         یادگیری ماشین مجموعه‌ای از روش‌های گوناگون است که کامپیوترها را قادر می‌سازد از داده‌ها بیاموزند و بر اساس آموخته‌هایشان تصمیم بگیرند. این آموخته‌ها شامل الگوها و روابط پیچیده‌ای است که در حجم انبوهی از داده‌ها وجود دارند، ولی به وضوح قابل‌تشخیص نیستند.کامپیوترها قادر خواهند بود بدون دخالت انسان این الگوها را یاد بگیرند و از آن‌ها برای حل مسائلی استفاده کنند که تا به حال با آنها مواجه نشده اند. کامپیوتر در واقع با پردازش داده‌ها، به یک بازنمایی یا مدل از آن‌ها دست می‌یابد.
       </Typography>
-      <Typography width={600}>
-        محصولات مبتنی بر این مدل‌ها را در همه جا می‌توان مشاهده کرد؛ از سیستم‌های پیشنهاددهنده‌ی فیلم و موسیقی گرفته              تا دوربین‌های خوانش پلاک ماشین، نرم‌افزار تبدیل متن به صوت و عامل هوشمند تشخیص سرطان.
+      <Typography width={700}>
+        محصولات مبتنی بر این مدل‌ها را در همه جا می‌توان مشاهده کرد؛ از سیستم‌های پیشنهاددهنده‌ی فیلم و موسیقی گرفته تا دوربین‌های خوانش پلاک ماشین، نرم‌افزار تبدیل متن به صوت و عامل هوشمند تشخیص سرطان.
       </Typography>
       <Box mt={6} width={800} alignSelf={'start'}>
         <Image
@@ -57,34 +58,57 @@ const MachineLearning = new Step({
 const ComputerVision = new Step({
   parent: ArtificalInteligence,
   id: "ComputerVision",
-  x: -3600,
-  y: -2400,
+  x: -4000,
+  y: -1300,
   scale: 0.66,
   children:
     <Stack alignItems={'center'} justifyContent={'center'} width={'100%'} height={'100%'}>
-      <Typography variant="h2" gutterBottom>{'بینایی ماشین'}</Typography>
-      <Typography variant="h5" gutterBottom>{'Computer Vision'}</Typography>
-      <Typography width={600}>
-        یک تصویر دیجیتال از دید کامپیوتر، صرفا شامل مقادیر عددی رنگ پیکسل‌ها است. بینایی ماشین شاخه‌ای است که به کامپیوتر اجازه می‌دهد درک عمیق‌تری از این اعداد پیدا کند و اطلاعاتی مانند اشیای درونِ تصویر را استخراج کند؛ برای مثال در دیجی‌کالا، کاربران به زودی خواهند توانست با ارسال یک تصویر، کالاهای مشابه آن را از میان هزاران کالا پیدا کنند. شبکه‌های عصبی عمیق در پیشرفت این حوزه نقش به‌سزایی داشته‌اند. با این حال به علت نیاز به داده‌های زیاد در این شاخه، فراهم کردن میلیون‌ها تصویر با برچسب مناسب، همواره یک چالش بزرگ بوده است.
-      </Typography>
-      <Stack width={'100%'} direction={'row'} alignItems={'center'} justifyContent={'space-evenly'}>
-        <Box mt={4} width={500} alignSelf={'start'}>
-          <Image
-            width={0} height={0} src={CVImage} alt=""
-            style={{ width: '100%', height: 'auto' }} />
-        </Box>
-        <PlayGameButton gameLink={'https://neural-network-playground.darkube.app/#activation=sigmoid&batchSize=10&dataset=circle&regDataset=reg-plane&learningRate=0.15&regularizationRate=0&noise=0&networkShape=3,1&seed=0.95066&showTestData=false&discretize=false&percTrainData=50&x=true&y=true&xTimesY=false&xSquared=false&ySquared=false&cosX=false&sinX=false&cosY=false&sinY=false&collectStats=false&problem=classification&initZero=false&hideText=false&showTestData_hide=true&activation_hide=true&noise_hide=true&resetButton_hide=false&regularization_hide=true&dataset_hide=false&playButton_hide=false&learningRate_hide=true&regularizationRate_hide=true&percTrainData_hide=true&discretize_hide=true&batchSize_hide=true&stepButton_hide=false&problem_hide=true'} />
+      <Stack width={700}>
+        <Typography variant="h2" gutterBottom>{'بینایی ماشین'}</Typography>
+        <Typography variant="h5" gutterBottom>{'Computer Vision'}</Typography>
+        <Typography>
+          یک تصویر دیجیتال از دید کامپیوتر، صرفا شامل مقادیر عددی رنگ پیکسل‌ها است. بینایی ماشین شاخه‌ای است که به کامپیوتر اجازه می‌دهد درک عمیق‌تری از این اعداد پیدا کند و اطلاعاتی مانند اشیای درونِ تصویر را استخراج کند؛ برای مثال در دیجی‌کالا، کاربران به زودی خواهند توانست با ارسال یک تصویر، کالاهای مشابه آن را از میان هزاران کالا پیدا کنند. شبکه‌های عصبی عمیق در پیشرفت این حوزه نقش به‌سزایی داشته‌اند. با این حال به علت نیاز به داده‌های زیاد در این شاخه، فراهم کردن میلیون‌ها تصویر با برچسب مناسب، همواره یک چالش بزرگ بوده است.
+        </Typography>
       </Stack>
-
+      <Stack mt={4} width={700} direction={'row'} alignItems={'center'} justifyContent={'space-between'}>
+        <Image width={0} height={0} src={CVImage} style={{ width: 600, height: 'auto' }} alt="" />
+        <Box mr={-20}>
+          <PlayGameButton gameLink={'https://quick-draw.darkube.app/'} />
+        </Box>
+      </Stack>
     </Stack>,
   links: [
     // { x: 650, y: 520, rotate: 90, length: 400 }
   ]
 });
 
+const NaturalLanguageProcessing = new Step({
+  parent: ArtificalInteligence,
+  id: "NaturalLanguageProcessing",
+  x: -3400,
+  y: 2650,
+  scale: 0.66,
+  children:
+    <Stack alignItems={'center'} justifyContent={'center'} width={'100%'} height={'100%'}>
+      <Typography variant="h2" gutterBottom>{'پردازش زبان طبیعی'}</Typography>
+      <Stack width={600} paddingLeft={15}>
+        <Typography alignSelf={'end'} variant="h5" gutterBottom>{'Natural Language Processing'}</Typography>
+        <Typography>
+          پردازش زبان طبیعی، شاخه‌ای میان‌رشته‌ای بین هوش مصنوعی و زبان‌شناسی است. هدف آن ساخت سیستم ایده‌آلی است که زبان طبیعی انسان را با تمام پیچیدگی‌های آن درک کند و با تشخیص ساختارهای زبانی و استخراج اطلاعات مهم از متن، پاسخ مناسبی به مسائل ارائه دهد. طی هفتاد سال اخیر هسته‌ی این سیستم‌ها از مجموعه‌ی بزرگی از قواعد زبانیِ دست‌نوشته به مدل‌های یادگیری ماشین تغییر یافته‌اند.
+        </Typography>
+      </Stack>
+      <Stack width={'100%'} direction={'row'} alignItems={'start'}>
+        <Image width={0} height={0} src={NLPImage} style={{ width: 500, height: 'auto' }} alt="" />
+      </Stack>
+    </Stack>,
+  links: [
+    // { x: 650, y: 520, rotate: 90, length: 400 }
+  ]
+});
 
 export default [
   ArtificalInteligence,
   MachineLearning,
   ComputerVision,
+  NaturalLanguageProcessing,
 ];
