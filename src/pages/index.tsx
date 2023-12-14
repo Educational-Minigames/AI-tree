e8a668f235ba9ae02c3961d041860ad0256e6004
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-12-14 08:38:56 +0330

    feaeture: - add home button - add initial dialog

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,12 @@
 import Step from "components/molecules/Step";
 import React, { Fragment, useEffect, useState } from "react";
 import steps from 'components/organisms/StepsData';
-import { Button, Dialog, Stack, Typography } from "@mui/material";
+import { Box, Button, Dialog, Stack, Typography, Backdrop, IconButton } from "@mui/material";
+import HomeIcon from '@mui/icons-material/Home';
 
 function Home() {
   const [impress, setImpress] = useState<any>();
-  const [openInitialDialog, setOpenInitialDialog] = useState(false);
+  const [openInitialDialog, setOpenInitialDialog] = useState(true);
 
   useEffect(() => {
     const impress = (window as any).impress();
@@ -17,17 +18,41 @@ function Home() {
 
   return (
     <Fragment>
-      <Dialog open={openInitialDialog} onClose={() => setOpenInitialDialog(!openInitialDialog)}>
+      <Dialog
+        sx={{ background: '#0F3C6CA0' }}
+        maxWidth='xs'
+        PaperComponent={Stack}
+        open={openInitialDialog}>
         <Stack width={'100%'}>
-          <Typography variant="h3">
+          <Typography align="left" variant="h3" gutterBottom>
             {'AI Tree'}
           </Typography>
-          <Typography variant="h3">
+          <Typography align="right" variant="h3" gutterBottom>
             {'درخت هوش مصنوعی'}
           </Typography>
-          <Typography>
-            فناوری هوش مصنوعی همچون درختی از روش‌ها و الگوریتم‌های گوناگون است که تازه در قرن بیست و یکم به بار نشسته است. بیایید گردشی کوتاه داشته باشیم در میان اصلی‌ترین شاخه‌های کنونی این فناوری.
+          <Typography gutterBottom>
+            فناوری هوش مصنوعی همچون درختی از روش‌ها و الگوریتم‌های گوناگون است که تازه در قرن بیست و یکم به بار نشسته است. بیایید گردشی کوتاه داشته باشیم در میان اصلی‌ترین شاخه‌های کنونی این فناوری...
           </Typography>
+          <Stack direction={'row'} width={'100%'} mt={3} justifyContent={'space-between'}>
+            <Button onClick={() => {
+              setOpenInitialDialog(!openInitialDialog);
+              impress.goto('OverallView', 1000);
+            }}
+              variant="contained" sx={{
+                fontSize: 18, backgroundColor: '#F5EFD9', color: '#0F3C6C',
+              }}>
+              {'کاوش کنید'}
+            </Button>
+            <Button onClick={() => {
+              setOpenInitialDialog(!openInitialDialog);
+              impress.goto('ArtificalInteligence', 1000);
+            }}
+              variant="contained" sx={{
+                fontSize: 18, backgroundColor: '#F5EFD9', color: '#0F3C6C',
+              }}>
+              {'طبق الگو بروید'}
+            </Button>
+          </Stack>
         </Stack>
       </Dialog>
       <div id="impress" data-transition-duration="1000">
@@ -37,24 +62,29 @@ function Home() {
         })}
       </div>
       <script type="text/javascript" src="/js/impress.js" />
-      <Button
-        onClick={() => impress.prev()}
-        size="large" variant='contained' sx={{
-          boxShadow: '-3px 4px 4.159999847412109px 0px #00000059',
-          backgroundColor: '#0F3C6C', color: '#F5EFD9', fontSize: 30,
-          position: 'fixed', bottom: 30, left: 30, width: 180,
-        }}>
-        {'بازگشت'}
-      </Button>
-      <Button
-        onClick={() => impress.next()} 
-        size="large" variant='contained' sx={{
-          boxShadow: '3px 4px 4.159999847412109px 0px #00000059',
-          backgroundColor: '#0F3C6C', color: '#F5EFD9', fontSize: 30,
-          position: 'fixed', bottom: 30, right: 30, width: 180,
-        }}>
-        {'ادامه'}
-      </Button>
+      <Box display={openInitialDialog ? 'none' : 'inherit'}>
+        <IconButton disableRipple size="large" onClick={() => impress.goto('OverallView', 1000)}>
+          <HomeIcon sx={{ fontSize: 50 }} />
+        </IconButton>
+        <Button
+          onClick={() => impress.prev()}
+          size="large" variant='contained' sx={{
+            boxShadow: '-3px 4px 4px 0px #00000059',
+            backgroundColor: '#0F3C6C', color: '#F5EFD9', fontSize: 30,
+            position: 'fixed', bottom: 30, left: 30, width: 180,
+          }}>
+          {'بازگشت'}
+        </Button>
+        <Button
+          onClick={() => impress.next()}
+          size="large" variant='contained' sx={{
+            boxShadow: '3px 4px 4 0px #00000059',
+            backgroundColor: '#0F3C6C', color: '#F5EFD9', fontSize: 30,
+            position: 'fixed', bottom: 30, right: 30, width: 180,
+          }}>
+          {'ادامه'}
+        </Button>
+      </Box>
     </Fragment >
   )
 }
