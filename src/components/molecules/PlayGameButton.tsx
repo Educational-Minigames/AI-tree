c6ff6f0f91580f45cedcf2ac747f449cbce49ade
Seyed Alireza Hashemi <amoo.hashem.mehraban@gmail.com>
2023-12-14 09:03:34 +0330

    fix PlayGameButton style

--- src/components/molecules/PlayGameButton.tsx
+++ src/components/molecules/PlayGameButton.tsx
@@ -1,8 +1,8 @@
-import { IconButton } from "@mui/material";
+import { Button, Stack, Typography } from "@mui/material";
 import { FC, Fragment, useState } from "react";
-import PlayGame from 'assets/play-game-button.png';
 import Image from "next/image";
 import IframDialog from "../atoms/IframDialog";
+import PlayGameButtonIcon from 'assets/play-game-button-icon.png';
 
 type PlayGameButtonPropsType = {
   gameLink: string;
@@ -15,11 +15,17 @@ const PlayGameButton: FC<PlayGameButtonPropsType> = ({
 
   return (
     <Fragment>
-      <IconButton onClick={() => setOpen(!open)} disableRipple>
-        <Image
-          width={0} height={0} src={PlayGame} alt=""
-          style={{ width: '100%', height: 'auto' }} />
-      </IconButton>
+      <Button disableRipple onClick={() => setOpen(!open)} variant="contained" sx={{
+        paddingX: 2, paddingY: 1.8, backgroundColor: '#F5EFD9', borderRadius: 100,
+        boxShadow: '4px 4px 4px 0px #00000073',
+      }}>
+        <Stack alignItems={'center'} direction={'row'}>
+          <Typography fontSize={24} color={'#0F3C6C'} marginLeft={2} fontWeight={1000}>
+            {'بازی کنید'}
+          </Typography>
+          <Image width={0} height={0} style={{ width: 34, height: 34 }} src={PlayGameButtonIcon} alt="" />
+        </Stack>
+      </Button>
       <IframDialog src={gameLink} open={open} handleClose={() => setOpen(!open)} />
     </Fragment>
   )
